ccb6efa3c5a3f55a2e5576f7242e01dbbfd132d7
10yeony <10yeony@gmail.com>
2020-08-19 15:14:12 +0900

    [2020-08-19 15:14] add Annotation

--- src/main/java/com/devils/pics/controller/StudioInfoController.java
+++ src/main/java/com/devils/pics/controller/StudioInfoController.java
@@ -1,8 +1,13 @@
 package com.devils.pics.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devils.pics.service.StudioInfoService;
 
 @RestController
 public class StudioInfoController {
 
+	@Autowired
+	private StudioInfoService studioInfoService;
 }
--- src/main/java/com/devils/pics/dao/impl/StudioFilterDAOImpl.java
+++ src/main/java/com/devils/pics/dao/impl/StudioFilterDAOImpl.java
@@ -2,9 +2,11 @@ package com.devils.pics.dao.impl;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.devils.pics.dao.StudioFilterDAO;
 
+@Repository
 public class StudioFilterDAOImpl implements StudioFilterDAO {
 
 	@Autowired

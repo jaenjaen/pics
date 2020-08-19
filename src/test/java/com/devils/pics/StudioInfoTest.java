c725a0ae82c8ea17b1d694aee9210b97606f6e58
Jaeeun Lee <je028770@gmail.com>
2020-08-19 19:42:17 +0900

    [2020-08-19 19:42] session 변수변경

--- src/test/java/com/devils/pics/StudioInfoTest.java
+++ src/test/java/com/devils/pics/StudioInfoTest.java
@@ -5,14 +5,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.devils.pics.domain.Studio;
+
 @SpringBootTest
 class StudioInfoTest {
 
 	@Autowired
-	  private SqlSession session;
+	  private SqlSession sqlSession;
 	
 	@Test
 	void contextLoads() {
+		String NS = "StudioInfoMapper.";
+		Studio vo=sqlSession.selectOne(NS+"getStudioInfo",1);
+		System.out.println(vo);
 	}
 
 }

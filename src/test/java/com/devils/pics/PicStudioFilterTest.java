8dd7de535477497b0a2db44a57f25c52528465dd
KJY <wpdud001@gmail.com>
2020-08-19 15:35:43 +0900

    studio_filter 정말 초기 어노테이션을 받기 위한 커밋

--- src/test/java/com/devils/pics/PicStudioFilterTest.java
+++ src/test/java/com/devils/pics/PicStudioFilterTest.java
@@ -1,5 +1,12 @@
 package com.devils.pics;
 
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +14,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 class PicStudioFilterTest {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws IOException {
+		Reader r = Resources.getResourceAsReader("SqlMapConfig.xml");
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(r);
+		SqlSession session = factory.openSession();
+		
+		//showAllStudio
+		session.
 	}
 
 }

7f5f4e9990bb35a855826d156e8cfc480487724a
Jaeeun Lee <je028770@gmail.com>
2020-08-19 12:15:41 +0900

    [2020-08-19 12:14] edit version, add xml, java
    Mysql Connector version수정
    query파일 추가
    test 파일 추가

---
+++ src/test/java/com/devils/pics/PicCustomerTest.java
@@ -0,0 +1,13 @@
+package com.devils.pics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicCustomerTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

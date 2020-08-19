516ee96ebd029e86a5e434b8402f81f945baa4b0
10yeony <10yeony@gmail.com>
2020-08-19 19:12:19 +0900

    [2020-08-19 19:11] delete

--- src/test/java/com/devils/pics/StudioFilterTest.java
+++ src/test/java/com/devils/pics/StudioFilterTest.java
@@ -1,14 +1,11 @@
 package com.devils.pics;
 
-import java.util.ArrayList;
-
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.devils.pics.domain.StudioFilter;
-import com.devils.pics.domain.Tag;
 
 @SpringBootTest
 class StudioFilterTest {

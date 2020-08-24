7a094739133da8707dcb6df5bb916c2ab87cffb9
Jaeeun Lee <je028770@gmail.com>
2020-08-24 13:50:25 +0900

    [2020-08-24 13:50] 정리

--- src/main/java/com/devils/pics/controller/CustomerController.java
+++ src/main/java/com/devils/pics/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devils.pics.domain.Customer;
+import com.devils.pics.service.CompanyService;
 import com.devils.pics.service.CustomerService;
 
 @RestController
@@ -20,45 +21,5 @@ import com.devils.pics.service.CustomerService;
 public class CustomerController {
 
 	@Autowired
-	private CustomerService customerService;
-	
-	@PostMapping("/customer")
-	public ResponseEntity registerCustomer(@RequestBody Customer customer) {
-		try {
-			customerService.registerCustomer(customer);
-			return new ResponseEntity(HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity(HttpStatus.BAD_REQUEST);
-		}
-	}
-	
-	@GetMapping("/customer/{custId}")
-	public ResponseEntity getCustomer(@PathVariable int custId) {
-		try {
-			Customer cust = customerService.getCustomer(custId);
-			return new ResponseEntity(cust,HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity(HttpStatus.BAD_REQUEST);
-		}
-	}
-	
-	@PutMapping("/customer")
-	public ResponseEntity updateCustomer(@RequestBody Customer customer) {
-		try {
-			customerService.updateCustomer(customer);
-			return new ResponseEntity(HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity(HttpStatus.BAD_REQUEST);
-		}
-	}
-	
-	@DeleteMapping("/customer/{custId}")
-	public ResponseEntity deleteCustomer(@PathVariable int custId) {
-		try {
-			customerService.deleteCustomer(custId);
-			return new ResponseEntity(HttpStatus.OK);
-		} catch (Exception e) {
-			return new ResponseEntity(HttpStatus.BAD_REQUEST);
-		}
-	}
+	private CompanyService compnayService;
 }

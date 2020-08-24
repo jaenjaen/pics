5bd137e7697f2453efcf5f8bd876a12c597d4a79
Jaeeun Lee <je028770@gmail.com>
2020-08-24 15:27:14 +0900

    [2020-08-24 15:27] company controller 완성

--- src/main/java/com/devils/pics/dao/CompanyDAO.java
+++ src/main/java/com/devils/pics/dao/CompanyDAO.java
@@ -3,6 +3,10 @@ package com.devils.pics.dao;
 import com.devils.pics.domain.Company;
 
 public interface CompanyDAO {
-	public int registerCompany(Company company);
+	int registerCompany(Company company) throws Exception;
+	Company getCompany(Company company)throws Exception;
+	Company loginCompany(Company company) throws Exception;
+	int deleteCompany (String comId) throws Exception;
+	int updateCompnay (Company company) throws Exception;
 }
 
--- src/main/java/com/devils/pics/service/CompanyService.java
+++ src/main/java/com/devils/pics/service/CompanyService.java
@@ -3,6 +3,10 @@ package com.devils.pics.service;
 import com.devils.pics.domain.Company;
 
 public interface CompanyService {
-	public int registerCompany(Company company) throws Exception;
+	int registerCompany(Company company) throws Exception;
+	Company getCompany(Company company)throws Exception;
+	Company loginCompany(Company company) throws Exception;
+	int deleteCompany (String comId) throws Exception;
+	int updateCompnay (Company company) throws Exception;
 }
 

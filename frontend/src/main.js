c7b25cd156b93039b87b7bf4f098c830177c0586
Jaennie <je028770@gmail.com>
2020-08-26 19:36:51 +0900

    [2020-08-26 19:36] 주소입력 api 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VueSession from 'vue-session';
+import VueDaumPostcode from "vue-daum-postcode"
+import VModal from 'vue-js-modal'
+
+
 Vue.config.productionTip = false;
 new Vue({
     router,
@@ -12,4 +16,7 @@ new Vue({
 var sessionOptions = {
     persist: true
 };
+
 Vue.use(VueSession, sessionOptions);
+Vue.use(VueDaumPostcode);
+Vue.use(VModal)

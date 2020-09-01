621103f091af44b65312acd76a0c52c628cecd40
baleineindigo <39041622+baleineindigo@users.noreply.github.com>
2020-09-01 21:06:16 +0900

    [2020-09-01 09:06] Chart Js 척척학사 재은님의 수정버전

--- frontend/src/assets/js/ChartJs.js
+++ frontend/src/assets/js/ChartJs.js
@@ -1,9 +1,13 @@
-// import { Doughnut } from 'vue-chartjs'
+import { Line, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
 
-// export default {
-//     extends: Doughnut,
-//     props: ['chartdata', 'options'],
-//     mounted() {
-//         this.renderChart(data, options)
-//     }
-// }
+export default {
+    extends: Line,
+    mixins: [reactiveProp],
+    props: ['options'],
+    mounted() {
+        // this.chartData is created in the mixin.
+        // If you want to pass options please create a local options object
+        this.renderChart(this.chartData, this.options)
+    }
+}

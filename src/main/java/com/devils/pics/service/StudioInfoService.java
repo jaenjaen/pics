238780ccc6d350fd47c8f9f0e1d8e8c42016122d
10yeony <10yeony@gmail.com>
2020-09-02 23:10:41 +0900

    [2020-09-02 23:09] complete RegisterStudio function
    - 추후 예외 처리 추가 필요(같은 스튜디오 이름, 같은 스튜디오 주소일 경우 등록 못하게 막기)
    - 카테고리 목록 DB에서 가져와서 화면에 뿌리기

--- src/main/java/com/devils/pics/service/StudioInfoService.java
+++ src/main/java/com/devils/pics/service/StudioInfoService.java
@@ -18,8 +18,9 @@ public interface StudioInfoService {
 	public int addBookmark(Bookmark bookmark); //tag 추가
 	
 	/* 스튜디오 공간 등록 관련 메소드 */
-	public int getCategoryId(Category category); //카테고리 이름으로 Studio에 등록할 category_id(select value값으로 대체 가능할 듯)
 	public int registerStudioInfo(Studio studio); //Studio 등록
+	public int registerTag(Tag tag); //Tag 등록
+	public int getCategoryId(Category category); //카테고리 이름으로 Studio에 등록할 category_id(select value값으로 대체 가능할 듯)
 	public int getStudioId(Studio studio); //Studio 등록 후 StudioFilter에 넣을 stu_id(autoIncrement)를 찾아옴
 	
 	// 스튜디오 chart & graph

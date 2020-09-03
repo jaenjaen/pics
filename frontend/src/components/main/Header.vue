e3023b5f34012a01e7e231d9af15b1dded08c3ec
Ryanwolf <cksdud7158@naver.com>
2020-09-03 16:08:35 +0900

    메인페이지 전체 반응형 수정중/

--- frontend/src/components/main/Header.vue
+++ frontend/src/components/main/Header.vue
@@ -1,31 +1,32 @@
 <template>
-  <div>
-    <div class="navbar-fixed">
-      <nav>
-        <div class="nav-wrapper">
+  <div class="navbar-fixed">
+    <nav class="header_nav">
+      <div class="header_container">
+        <div>
           <a href>
-            <img src="@/assets/img/util/backward.svg" alt id="backward" @click.prevent="moveBack" />
-          </a>
-          <a href="http://localhost:9999">
-            <img
-              src="@/assets/img/header_bottom/pics_logo.svg"
-              alt="pics"
-              class="brand-logo center"
-              id="pics_logo"
-            />
-          </a>
-          <a href="#" @click.prevent="moveToSearch(-1)">
-            <img src="@/assets/img/header_bottom/search_icon.svg" class="right" id="search_icon" />
+            <img src="@/assets/img/util/backward.svg" id="backward" @click.prevent="moveBack" />
           </a>
         </div>
-      </nav>
-    </div>
-    <Bottom />
+        <div>
+          <a href="http://localhost:9999">
+            <img src="@/assets/img/header_bottom/pics_logo.svg" alt="pics" id="pics_logo" />
+          </a>
+        </div>
+        <div>
+          <a href="#" @click.prevent="moveToSearch(-1)">
+            <img src="@/assets/img/header_bottom/search_icon.svg" id="search_icon" />
+          </a>
+        </div>
+      </div>
+    </nav>
+    <!-- <Bottom /> -->
   </div>
 </template>
 
 <script src="@/assets/js/main/header.js">
 </script>
+<style scoped src="@/assets/css/remove_css.css">
+</style>
 <style scoped src="materialize-css/dist/css/materialize.min.css"></style>
 <style scoped src="@/assets/css/main/header.css">
 </style>

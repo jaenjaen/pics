a02d28742c6b185a04a32ac3d1cf6e8bcf175795
10yeony <10yeony@gmail.com>
2020-09-07 22:34:13 +0900

    [2020-09-07 22:32] seperate css, js from Chat

--- frontend/src/components/chat/Chat.vue
+++ frontend/src/components/chat/Chat.vue
@@ -1,261 +1,128 @@
+<script>
+    var wrapper = document.getElementById("chat-wrapper");
+    wrapper.scrollTop = wrapper.scrollHeight;
+</script>
+<script scoped src="@/assets/js/chat/Chat.js"></script>
 <template>
     <div id="app">
-        <div class='chat-wrapper'>
+        <!-- Header : 채팅하는 대상 -->
+        <header>
+            <div>닉네임</div>
+        </header>
+
+        <!-- 채팅 본문 : 대화 내용 -->
+        <div id="chat-wrapper" class='chat-wrapper'>
             <div class='chat-message padding'>
-            <div class='chat-message chat-message-recipient'>
-                <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user1.jpg' />
-                <div class='chat-message-wrapper'>
-                <div class='chat-message-content'>
-                    <p>Sushi tonight?</p>
-                </div>
-                <div class='chat-details'>
-                    <span class='chat-message-localization font-size-small'>My Address</span>
-                    <span class='chat-message-read-status font-size-small'>- Read</span>
-                </div>
-                </div>
-            </div>
-            <div class='chat-message chat-message-sender'>
-                <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user2.jpg' />
-                <div class='chat-message-wrapper'>
-                <div class='chat-message-content'>
-                    <p>Pizza?</p>
-                    <p>We had sushi last time!</p>
-                    <p>For me one Vegetariana! Plus some chili oil on it!</p>
-                </div>
-                <div class='chat-details'>
-                    <span class='chat-message-localisation font-size-small'>My Address</span>
-                    <span class='chat-message-read-status font-size-small'>- Read</span>
-                </div>
+              <div class='chat-message chat-message-recipient'>
+                  <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user1.jpg' />
+                  <div class='chat-message-wrapper'>
+                  <div class='chat-message-content'>
+                      <p>Sushi tonight?</p>
+                  </div>
+                  <div class='chat-details'>
+                      <span class='chat-message-localization font-size-small'>My Address</span>
+                      <span class='chat-message-read-status font-size-small'>- Read</span>
+                  </div>
+                  </div>
+              </div>
+              <div class='chat-message chat-message-sender'>
+                  <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user2.jpg' />
+                  <div class='chat-message-wrapper'>
+                  <div class='chat-message-content'>
+                      <p>Pizza?</p>
+                      <p>We had sushi last time!</p>
+                      <p>For me one Vegetariana! Plus some chili oil on it!</p>
+                  </div>
+                  <div class='chat-details'>
+                      <span class='chat-message-localisation font-size-small'>My Address</span>
+                      <span class='chat-message-read-status font-size-small'>- Read</span>
+                  </div>
 
-                </div>
-            </div>
-            <div class='chat-message chat-message-recipient'>
-                <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user1.jpg' />
+                  </div>
+              </div>
+              <div class='chat-message chat-message-recipient'>
+                  <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user1.jpg' />
 
-                <div class='chat-message-wrapper'>
-                <div class='chat-message-content'>
-                    <p>Done. Delivery takes 5 minutes!</p>
-                </div>
-                <div class='chat-details'>
-                    <span class='chat-message-localisation font-size-small'>My Address</span>
-                    <span class='chat-message-read-status font-size-small'>- Read</span>
-                </div>
-                </div>
+                  <div class='chat-message-wrapper'>
+                  <div class='chat-message-content'>
+                      <p>Done. Delivery takes 5 minutes!</p>
+                  </div>
+                  <div class='chat-details'>
+                      <span class='chat-message-localisation font-size-small'>My Address</span>
+                      <span class='chat-message-read-status font-size-small'>- Read</span>
+                  </div>
+                  </div>
+              </div>
+              <div class='chat-message chat-message-sender'>
+                  <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user2.jpg' />
+                  <div class='chat-message-wrapper'>
+                  <div class='chat-message-content'>
+                      <p>Awesome! See you later! : )</p>
+                  </div>
+                  <div class='chat-details'>
+                      <span class='chat-message-localization font-size-small'>My Address</span>
+                      <span class='chat-message-read-status font-size-small'>- Read</span>
+                  </div>
+                  </div>
+              </div>
+              <div class='chat-message chat-message-sender'>
+                  <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user2.jpg' />
+                  <div class='chat-message-wrapper'>
+                  <div class='chat-message-content'>
+                      <p>Awesome! See you later! : )</p>
+                  </div>
+                  <div class='chat-details'>
+                      <span class='chat-message-localization font-size-small'>My Address</span>
+                      <span class='chat-message-read-status font-size-small'>- Read</span>
+                  </div>
+                  </div>
+              </div>
+              <div class='chat-message chat-message-sender'>
+                  <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user2.jpg' />
+                  <div class='chat-message-wrapper'>
+                  <div class='chat-message-content'>
+                      <p>Awesome! See you later! : )</p>
+                  </div>
+                  <div class='chat-details'>
+                      <span class='chat-message-localization font-size-small'>My Address</span>
+                      <span class='chat-message-read-status font-size-small'>- Read</span>
+                  </div>
+                  </div>
+              </div>
             </div>
-            <div class='chat-message chat-message-sender'>
-                <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user2.jpg' />
-                <div class='chat-message-wrapper'>
-                <div class='chat-message-content'>
-                    <p>Awesome! See you later! : )</p>
-                </div>
-                <div class='chat-details'>
-                    <span class='chat-message-localization font-size-small'>My Address</span>
-                    <span class='chat-message-read-status font-size-small'>- Read</span>
-                </div>
-                </div>
+        </div>
+
+        <!-- Footer : 파일첨부, 메세지, 전송 -->
+        <footer>
+          <div style="background:white; display:inline-block;">
+            <span>
+              <a href="javascript:;" @click="showUploadModal">
+                <img src="http://localhost:7777/upload/default/add.png" width="30px" height="30px">
+              </a>
+            </span>
+            <span>
+                <input type="text" id="word" name="word">
+            </span>
+            <span>
+                <a href="javascript:;" @click="sendMsg">
+                  <img src="http://localhost:7777/upload/default/send.png" width="30px" height="30px">
+                </a>
+            </span>
+          </div>
+        </footer>
+
+        <!-- Modal : 파일 업로드 -->
+        <div id="uploadModal" style="display:none;">
+            <div id="uploadContent">
+                <div id="closeUpload" @click="hideUploadModal">&times;</div><br/><br/>
+                <p style="font-size:18px;">파일 첨부</p>
+                <input type="file" id="chatFileUpload" style="display:none;"><br/>
+                <input type="text" id="chatFileName" disabled>
+                <button id="uploadBtn" onclick="document.getElementById('chatFileUpload').click()">찾기</button>
+                <button id="removeBtn">삭제</button><br/>
+                <button id="sendBtn">전송</button>
             </div>
-            <div class='chat-message chat-message-sender'>
-                <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user2.jpg' />
-                <div class='chat-message-wrapper'>
-                <div class='chat-message-content'>
-                    <p>Awesome! See you later! : )</p>
-                </div>
-                <div class='chat-details'>
-                    <span class='chat-message-localization font-size-small'>My Address</span>
-                    <span class='chat-message-read-status font-size-small'>- Read</span>
-                </div>
-                </div>
-            </div>
-            <div class='chat-message chat-message-sender'>
-                <img class='chat-image chat-image-default' src='http://www.michaelmammoliti.com/_projects/chat/_media/img/user2.jpg' />
-                <div class='chat-message-wrapper'>
-                <div class='chat-message-content'>
-                    <p>Awesome! See you later! : )</p>
-                </div>
-                <div class='chat-details'>
-                    <span class='chat-message-localization font-size-small'>My Address</span>
-                    <span class='chat-message-read-status font-size-small'>- Read</span>
-                </div>
-                </div>
-            </div>
-            </div>
-            <footer>
-                <span>
-                    <img src="http://localhost:7777/upload/default/add.png" width="20px" height="20px">
-                </span>
-                <span>
-                    <input type="text" id="word" name="word">
-                </span>
-                <span>
-                    <img src="http://localhost:7777/upload/default/send.png" width="20px" height="20px">
-                </span>
-            </footer>
         </div>
     </div>
 </template>
-<style scoped>
-@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:300);
-
-* {
-  padding: 0;
-  margin: 0;
-  border: 0;
-  color: #888;
-  font-family: "Source Sans Pro", sans-serif;
-  font-weight: 300;
-  outline: none;
-  box-sizing: border-box;
-}
-#app {
-    padding: 50px;
-}
-.padding {
-  padding: 20px;
-}
-
-.clearfix {
-  display: table;
-  overflow: hidden;
-}
-.clearfix:before {
-  content: " ";
-  display: block;
-}
-
-.font-size-small {
-  font-size: 0.7rem;
-}
-
-.chat-wrapper {
-  width: 100%;
-  display: table;
-  overflow: hidden;
-}
-.chat-wrapper:before {
-  content: " ";
-  display: block;
-}
-.chat-wrapper .chat-content {
-  background-color: #fff;
-}
-.chat-wrapper .chat-image {
-  float: left;
-  border-radius: 50%;
-}
-.chat-wrapper .chat-image.chat-image-default {
-  width: 60px;
-  height: 60px;
-}
-.chat-wrapper .chat-message {
-  margin-bottom: 20px;
-  width: 100%;
-  display: table;
-  overflow: hidden;
-}
-.chat-wrapper .chat-message:before {
-  content: " ";
-  display: block;
-}
-.chat-wrapper .chat-message:last-of-type {
-  margin-bottom: 0;
-}
-.chat-wrapper .chat-message .chat-message-wrapper {
-  max-width: 70%;
-  display: table;
-  margin: 0 20px;
-  padding-top: 10px;
-  position: relative;
-}
-.chat-wrapper .chat-message .chat-message-wrapper:before {
-  content: " ";
-  display: block;
-  width: 0;
-  height: 0;
-  border: 12px solid transparent;
-  top: 20px;
-  position: absolute;
-  z-index: 2;
-}
-.chat-wrapper .chat-message .chat-message-wrapper p {
-  padding: 10px 20px;
-  color: #fff;
-  border-top: 1px solid;
-}
-.chat-wrapper .chat-message .chat-message-wrapper p:first-of-type {
-  border-top: 0 !important;
-}
-.chat-wrapper .chat-message.chat-message-recipient .chat-message-wrapper,
-.chat-wrapper .chat-message.chat-message-recipient .chat-message-content {
-  float: left;
-}
-.chat-wrapper .chat-message.chat-message-recipient .chat-message-wrapper:before {
-  left: -20px;
-  border-right-color: #91D98A;
-}
-.chat-wrapper .chat-message.chat-message-recipient p {
-  background-color: #91D98A;
-  border-top-color: #7dc576;
-}
-.chat-wrapper .chat-message.chat-message-sender .chat-message-wrapper,
-.chat-wrapper .chat-message.chat-message-sender .chat-message-content {
-  float: right;
-}
-.chat-wrapper .chat-message.chat-message-sender .chat-message-wrapper:before {
-  right: -20px;
-  border-left-color: #8AC0D9;
-}
-.chat-wrapper .chat-message.chat-message-sender p {
-  background: #8AC0D9;
-  border-top-color: #76acc5;
-}
-.chat-wrapper .chat-message.chat-message-sender img {
-  float: right;
-}
-.chat-wrapper .chat-message .chat-message-content {
-  display: table;
-  overflow: hidden;
-  border-radius: 4px;
-}
-.chat-wrapper .chat-message .chat-message-content:before {
-  content: " ";
-  display: block;
-}
-.chat-wrapper .chat-message .chat-details {
-  clear: both;
-  width: 100%;
-  display: table;
-  overflow: hidden;
-}
-.chat-wrapper .chat-message .chat-details:before {
-  content: " ";
-  display: block;
-}
-.chat-wrapper .chat-message .chat-details span {
-  margin-right: 4px;
-  color: #bbb;
-}
-footer{
-    display: block;
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 110px;
-    padding: 15px 0;
-    margin: 0 auto;
-    text-align: center;
-}
-/* 반응형 : 너비 500px 이하 */
-@media screen and (max-width: 500px) {
-    /* 프로필 사진 크기 조정 */
-    .chat-wrapper .chat-image.chat-image-default {
-        width: 30px;
-        height: 30px;
-    }
-}
-</style>
-<script>
-export default {
-    name: 'chat',
-}
-</script>
+<style scoped src="@/assets/css/chat/Chat.css"></style>

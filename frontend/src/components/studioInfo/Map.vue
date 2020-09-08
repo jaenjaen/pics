2c4994ed676702c61bae81a7f08cfaf51b996506
baleineindigo <39041622+baleineindigo@users.noreply.github.com>
2020-09-08 22:43:11 +0900

    [2020-09-08 22:43] 지도 중간 저장

--- frontend/src/components/studioInfo/Map.vue
+++ frontend/src/components/studioInfo/Map.vue
@@ -1,9 +1,18 @@
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=91cbdca7243fe89cb44e5d61a5aaaf44"></script>
 <template>
- <div id="map"></div>
+ <!-- <div id="map"></div> -->
+ <vue-daum-map
+      :appKey="appKey"
+      :center.sync="center"
+      :level.sync="level"
+      :mapTypeId="mapTypeId"
+      :libraries="libraries"
+      @load="onLoad"
+       style="width:500px;height:400px;"/>
 </template>
 <script>
-import VueDaumMap from 'vue-daum-map'
-import loadScriptOnce from 'load-script-once';
+import VueDaumMap from 'vue-daum-map';
+// import loadScriptOnce from 'load-script-once';
   const EVENTS = [
     'center_changed',
     'zoom_start',
@@ -20,7 +29,6 @@ import loadScriptOnce from 'load-script-once';
     'tilesloaded',
     'maptypeid_changed'
   ];
-
   //   const MapTypeId = {
   //   "ROADMAP": 1,
   //   "NORMAL": 1,
@@ -37,17 +45,18 @@ import loadScriptOnce from 'load-script-once';
   export default {
     name: "VueDaumMap",
     props: {
-      // appKey: {
-      //   type: String,
-      //   required: true,
-      // },
-      libraries: {
-        type: Array,
-        default: () => []
+      appKey: {
+        type: String,
+        required: true,
+        default:"0e41243b4bb80fef37c1383bd1bdcb7c"
       },
+      // libraries: {
+      //   type: Array,
+      //   default: () => ["services","clusterer","drawing"]
+      // },
       center: {
         type: Object,
-        required: true
+        required: true,
       },
       level: {
         type: Number,
@@ -80,11 +89,11 @@ import loadScriptOnce from 'load-script-once';
     },
     data: () => ({
         // appKey: '0e41243b4bb80fef37c1383bd1bdcb7c', // 테스트용 appkey
-        // center: {lat:33.450701, lng:126.570667}, // 지도의 중심 좌표
-        // libraries:[],
+        // center: {lat:37.4797302, lng:126.9237447}, // 지도의 중심 좌표
+        // libraries:["services","clusterer","drawing"],
         // level: 3, // 지도의 레벨(확대, 축소 정도),
-        // mapTypeId: VueDaumMap.MapTypeId.NORMAL, // 맵 타입
-        map: null // 지도 객체. 지도가 로드되면 할당됨.
+        // mapTypeId:1, // 맵 타입
+        // map: null // 지도 객체. 지도가 로드되면 할당됨.
     }),
     mounted () {
         loadScriptOnce(`//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=0e41243b4bb80fef37c1383bd1bdcb7c&libraries=services,clusterer,drawing`)
@@ -97,13 +106,11 @@ import loadScriptOnce from 'load-script-once';
           // });
           var container = document.getElementById('map'),
               options = {
-                  center: new kakao.maps.LatLng(33.450701, 126.570667),
+                  center: new kakao.maps.LatLng("37.4797302", "126.9237447"),
                   level: 3
               };
-          
-          var map = new kakao.maps.Map(container, options);
-
-          kakao.maps.load(function() {
+              var map = new kakao.maps.Map(container, options);
+              kakao.maps.load(function() {
               // v3가 모두 로드된 후, 이 콜백 함수가 실행됩니다.
               var map = new kakao.maps.Map(node, options);
           });
@@ -112,28 +119,12 @@ import loadScriptOnce from 'load-script-once';
           console.error(err);
            });
         },
-      watch: {
-        level () {
-          if (!this.map) {
-            return "Nothing Map Area";
-          }
-          this.map.setLevel(this.level);
-        },
-        center : {
-          handler () {
-            if (!this.map) {
-              return;
-            }
-            this.map.setCenter(new daum.maps.LatLng(this.center.lat, this.center.lng));
-          },
-          deep: true
-        }
-      },
     methods: {
-          onLoad (map) {
-            this.map = map
-        },
-      render () {
+    onLoad (map) {
+      alert("map : "+map);
+      this.map = map
+      },
+    render () {
         console.log("render.start~!!");
         const options = { //지도를 생성할 때 필요한 기본 옵션
           center: new daum.maps.LatLng(this.center.lat, this.center.lng), //지도의 중심좌표.
@@ -176,7 +167,6 @@ import loadScriptOnce from 'load-script-once';
           lng: latlng.getLng()
         });
       }
-
     },
     // MapTypeId: this.MapTypeId
   }

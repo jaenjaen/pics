7a6df10bc8c9be1284bf490f4abb7cf6a06d12e0
Ryanwolf <cksdud7158@naver.com>
2020-09-08 09:51:13 +0900

    company_info 컴포넌트 추가/ 스튜디오 필터 쿼리문에서 com_id 받아오는거 추가

---
+++ frontend/src/components/company/company_info.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{comId}}</div>
+</template>
+
+<script src="@/assets/js/company/company_info.js"></script>
+
+<style scoped src="@/assets/css/remove_css.css">
+</style>
+<style scoped src="materialize-css/dist/css/materialize.min.css">
+</style>
+<style scoped src="@/assets/css/company/company_info.css">
+</style>

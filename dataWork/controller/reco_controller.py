7065b4d644c9c4714d86560bf64c1dc08b911acf
KJY <wpdud001@gmail.com>
2020-09-15 10:53:21 +0900

    [2020-09-15 10:53] 추천알고리즘 90% 완성

--- dataWork/controller/reco_controller.py
+++ dataWork/controller/reco_controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[13]:
+# In[ ]:
 
 
 # 외부 모듈
@@ -16,6 +16,146 @@ def recommend():
     return df.iat[2,1] # 서버에서 받는지 확인용.... df가 진짜 리턴값
 
 
+# In[1]:
 
 
+import pandas as pd
+import numpy as np
+
+import sys
+sys.path.append('..')
+from model.reco_model import *
+
+
+# In[2]:
+
+
+
+
+data = pd.read_excel('review.xlsx', enconding='utf-8')
+data.rename({'studio_name':'stu_id'}, axis='columns', inplace=True)
+data[data.duplicated(['stu_id','cust_id'])]
+data.drop_duplicates(['stu_id','cust_id'], inplace=True)
+data.score = round(data.score,0)
+
+
+# In[3]:
+
+
+
+ratings = data.pivot_table(index='cust_id', columns='stu_id', values='score')
+index_list = [i+1 for i in range(len(ratings.index))]
+ratings.index = index_list
+ratings.columns = index_list
+ratings.index.name = 'cust_id'
+ratings.columns.name = 'stu_id'
+ratings.head()
+# ratings.isnull().sum().sum()
+
+
+# In[4]:
+
+
+ratings_i=injection(ratings, 0.2)
+
+
+# In[ ]:
+
+
+ratings
+
+
+# In[ ]:
+
+
+table=get_cossine_similarity_table(ratings_i)
+ratings_p = pred_scores(ratings_i, table)
+
+
+# In[ ]:
+
+
+ratings_p.iloc[6:15]
+
+
+# In[ ]:
+
+
+ratings_i.iloc[6:15]
+
+
+# In[ ]:
+
+
+
+# from itertools import product
+# import surprise
+# from surprise.model_selection import cross_validate
+# data = pd.read_excel("test_reco2.xlsx", encoding='utf-8')
+# data = surprise.Dataset.load_builtin('ml-100k')
+# df = pd.DataFrame(data.raw_ratings, columns=["user", "item", "rate", "id"])
+# ratings = df.pivot_table(index='user', columns='item', values='rate')
+# ratings_s = ratings.iloc[:20,:40]
+
+
+# In[ ]:
+
+
+get_ipython().system('jupyter nbconvert --to script reco_controller.ipynb ')
+
+
+# In[ ]:
+
+
+# #surprise 모듈 사용
+
+
+# reader = surprise.Reader(rating_scale=(1,5))
+# ds = surprise.Dataset.load_from_df(ratings_r.loc[:,['user','item', 'score']],reader)
+
+# sim_options = {'name':'cossine',
+#               'user_based':False}
+# # sim_options = {'name':'pearson'}
+# # algo = surprise.KNNBasic(sim_options=sim_options)
+# algo = surprise.KNNBasic(sim_options=sim_options)
+# print(cross_validate(algo, ds)['test_rmse'].mean())
+# ds_set = ds.build_full_trainset()
+# algo.fit(ds_set)
+
+# # index_combinations=list(product(final['userId'], final['itemId'])) # repeat = n 씩 조합한다
+# # index_combinations
+# # for userId, itemId in index_combinations:
+# # #     print(type(userId),itemId)
+# # #     predict_list = []
+# #     predict_rate = algo.predict(userId, itemId).est
+# # #     print(algo.predict(userId,itemId).est, algo.predict(userId,itemId).details.get('was_impossible'))
+# #     predict_dic = {
+# #         'userId':userId,
+# #         'itemId':itemId,
+# #         'rate': predict_rate
+# #     }
+# #     predict_list.append(predict_dic)
+# # predict_list
+# # rating_result = pd.DataFrame(predict_list)
+# # rating_result
+
+
+# In[ ]:
+
+
+# # (4) CF 진행
+# reader = surprise.Reader(rating_scale=(1,5))
+# ds = surprise.Dataset.load_from_df(compare.loc[:,['userId','itemId', 'rate']],reader)
+
+# sim_options = {'name':'pearson',
+#               'user_based':False}
+# # sim_options = {'name':'pearson'}
+# # algo = surprise.KNNBasic(sim_options=sim_options)
+# algo = surprise.KNNBasic(sim_options=sim_options)
+# print(cross_validate(algo, ds)['test_rmse'].mean())
+# ds_set = ds.build_full_trainset()
+# algo.fit(ds_set)
+# # for i in ratings.index:
+# #     for j in ratings.columns:
+# #         print("{}" .format(algo.predict(i,j)))
 
--- dataWork/dao/reco_dao.py
+++ dataWork/dao/reco_dao.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[29]:
+# In[3]:
 
 
 import pymysql
 import pandas as pd
+
+
+# In[ ]:
+
 
 #DB 접근
 def get_cfsource() :
@@ -17,17 +21,8 @@ def get_cfsource() :
                          charset='utf8',
                        cursorclass=pymysql.cursors.DictCursor)
     cursor= db.cursor()
-    sql = "SELECT s.stu_id stuId, r.score score, r.cust_id custId "
-    sql += "FROM studio s "
-    sql += "LEFT OUTER JOIN studio_filter sf "
-    sql += "ON s.stu_id = sf.stu_id "
-    sql += "LEFT OUTER JOIN review r "
-    sql += "ON s.stu_id = r.stu_id "
-    sql += "UNION "
-    sql += "SELECT r.stu_id studId, r.score score, cust.cust_id custId "
-    sql += "FROM customer cust "
-    sql += "LEFT OUTER JOIN review r "
-    sql += "ON cust.cust_id = r.cust_id "
+    sql = "SELECT cust_id, stu_id, score "
+    sql += "FROM review"
 
     cursor.execute(sql)
     db.commit()
@@ -36,5 +31,83 @@ def get_cfsource() :
     db.close()
     df = pd.DataFrame(result)
     return df
+
+
+# In[4]:
+
+
+# data = pd.read_excel('review.xlsx', enconding='utf-8')
+# data.rename({'studio_name':'stu_id'}, axis='columns', inplace=True)
+# data[data.duplicated(['stu_id','cust_id'])]
+# data.drop_duplicates(['stu_id','cust_id'], inplace=True)
+# data.score = round(data.score,0)
+# ratings = data.pivot_table(index='cust_id', columns='stu_id', values='score')
+# ratings_d = data.pivot_table(index='cust_id', columns='stu_id', values='score')
+# index_list = [i+1 for i in range(len(ratings.index))]
+# ratings.index = np.arange(22, 42, 1)
+# ratings.columns = np.arange(1156, 1176, 1)
+# ratings.index.name = 'cust_id'
+# ratings.columns.name = 'stu_id'
+# ratings
+
+
+# In[13]:
+
+
+# from itertools import product
+# id_combo=list(product(ratings.index, ratings.columns))
+# res_id = 62
+# for cust_id, stu_id in id_combo:
+#     db= pymysql.connect(host='pics.crvbvpzlygpt.ap-northeast-2.rds.amazonaws.com',
+#                      port=3306,
+#                      user='pics',
+#                      passwd='picspics!1',
+#                      db='pics',
+#                      charset='utf8',
+#                    cursorclass=pymysql.cursors.DictCursor)
+#     cursor= db.cursor()
+#     sql = "INSERT INTO reservation(cust_id,stu_id,start_date,end_date,total_price,res_date,total_people) "
+#     sql += "VALUES("
+#     sql += str(cust_id)+", "
+#     sql += str(stu_id)+", "
+#     sql += "'2020-09-14', '2020-09-15', 100000, '2020-09-01', 10);"
+
+
+#     cursor.execute(sql)
+#     db.commit()
+#     db.close()
+
+
+# In[6]:
+
+
+# from itertools import product
+# id_combo=list(product(ratings.index, ratings.columns))
+# res_id = 2485
+# for cust_id, stu_id in id_combo:
+#     score = ratings.loc[cust_id,stu_id]
+#     if score > -1 :
+#         db= pymysql.connect(host='pics.crvbvpzlygpt.ap-northeast-2.rds.amazonaws.com',
+#                          port=3306,
+#                          user='pics',
+#                          passwd='picspics!1',
+#                          db='pics',
+#                          charset='utf8',
+#                        cursorclass=pymysql.cursors.DictCursor)
+#         cursor= db.cursor()
+#         sql = "INSERT INTO review(cust_id, stu_id, res_id, score) "
+#         sql += "VALUES( "
+#         sql += str(cust_id)+", "
+#         sql += str(stu_id)+", "
+#         sql += str(res_id)+", "
+#         sql += str(score) +"); "
+#     else : 
+#         continue
+
+#     print(sql)
+#     cursor.execute(sql)
+#     db.commit()
+#     db.close()
+#     res_id += 1
 
 

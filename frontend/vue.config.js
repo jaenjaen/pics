bd8d112f9e8b5e3a1f0b6850d4c0bb373cc772c6
10yeony <10yeony@gmail.com>
2020-09-16 22:22:36 +0900

    [2020-09-16 22:22] solve some error about chat
    - 채팅 목록 불러오거나 수신, 발신할 때 스크롤 최하단으로 내리기
    - 동일한 내용이 전송 후 여러번 출력되는 문제 해결(소켓 연결은 최초 한번만 함)

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,13 @@ module.exports = {
     transpileDependencies: ["vuetify"],
     outputDir: '../src/main/resources/static',
     devServer: {
-        port: 9999
+        public: 'localhost:9999',
+        host: 'localhost',
+        port: 9999,
+        disableHostCheck: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+        }
     }
 };
